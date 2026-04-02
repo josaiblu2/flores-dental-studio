c963ada292af31077dfd3f5191d456d8113a0335
josaiblu2 <josaiblu2@yahoo.com.mx>
2026-04-01 22:31:30 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
 export default defineConfig({
   output: 'static',
   integrations: [react()],
-  // Quitamos Tailwind temporalmente de la config para descartar errores de Vite
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
