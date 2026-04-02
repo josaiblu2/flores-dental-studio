3dc4c127a6664f4162afb71af5225744417418f7
josaiblu2 <josaiblu2@yahoo.com.mx>
2026-04-01 21:58:15 -0600

    Change output to static and remove Netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
-import netlify from '@astrojs/netlify'; // Importamos el adapter oficial
 
 export default defineConfig({
-  output: 'server', // Le decimos que sí, que use un servidor
-  adapter: netlify(), // Usamos el motor de Netlify
+  output: 'static', 
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()]
