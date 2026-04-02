20c7dff08553fce71150a83daef658ad1e8aab43
josaiblu2 <josaiblu2@yahoo.com.mx>
2026-04-01 21:45:48 -0600

    Change output to server and add Netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
+import netlify from '@astrojs/netlify'; // Importamos el adapter oficial
 
 export default defineConfig({
-  output: 'static', 
+  output: 'server', // Le decimos que sí, que use un servidor
+  adapter: netlify(), // Usamos el motor de Netlify
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()]
